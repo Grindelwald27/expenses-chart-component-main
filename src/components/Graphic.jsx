b425345f2a6e54ea9f229226438564bdd3deae1a
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-06-29 20:37:41 -0300

    continuo tentando fazer funcionar

--- src/components/Graphic.jsx
+++ src/components/Graphic.jsx
@@ -1,42 +1,15 @@
 import data from './data.js';
 import './Graphic.css';
+import './Amount.js';
 import './Amount.css';
 
 const Graphic = () => {
-
-    const divElement = document.querySelectorAll('.col');
-    const amount = document.querySelectorAll('.amount');
-
-    function over(){
-
-        for (let i = 0; i < divElement.length; i++) {
-
-            divElement[i].onmouseover = () => {
-
-                amount[i].style.display = 'initial';
-
-            }
-
-        }
-    }
-
-    function leave() {
-
-        for (let i = 0; i < divElement.length; i++) {
-
-            divElement[i].onmouseleave = () => {
-
-                amount[i].style.display = 'none';
-            }
-
-        }
-    }
 
     return (
         <>
             {data.map(data => {
                 return (
-                    <div className="col" key={data.id} onMouseOver={over()} onMouseLeave={leave()}>
+                    <div className="col" key={data.id}>
                         <div className="amount">${data.amount}</div>
                         <div id={data.day} className="column"></div>
                         <p>{data.day}</p>
